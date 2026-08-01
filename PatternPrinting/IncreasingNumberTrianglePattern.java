642ba0c48c49c5e3c4a1b060c1a328b2c92d0a7a
sauryaaman <amansaurya24@gmail.com>
2026-08-01 22:05:45 +0530

    Increasing Number Triangle Pattern

---
+++ PatternPrinting/IncreasingNumberTrianglePattern.java
@@ -0,0 +1,14 @@
+package PatternPrinting;
+
+public class IncreasingNumberTrianglePattern {
+    IncreasingNumberTrianglePattern(int n)
+    {
+        for (int i = 1; i <=n ; i++)
+        {
+            for (int j = 1; j <=i; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
--- PatternPrinting/testPattern.java
+++ PatternPrinting/testPattern.java
@@ -2,8 +2,11 @@ package PatternPrinting;
 
 public class testPattern {
     public static void main(String[] args) {
-//        RectangleofNSize r= new RectangleofNSize(5);
+         // RectangleofNSize r= new RectangleofNSize(5);
 
-        traingularpattern t= new traingularpattern(5);
+
+        //  traingularpattern t= new traingularpattern(5);
+
+          IncreasingNumberTrianglePattern increasingNumberTrianglePattern = new IncreasingNumberTrianglePattern(5);
     }
 }
