539c1d706548c8e54fbd064624a62c1be204b6ac
sauryaaman <amansaurya24@gmail.com>
2026-08-02 17:49:44 +0530

    updated some logic in Half Pyramid

--- PatternPrinting/HalfDiamond.java
+++ PatternPrinting/HalfDiamond.java
@@ -10,7 +10,7 @@ public class HalfDiamond {
             }
             System.out.println();
         }
-        for (int i =n; i >=1 ; i--) {
+        for (int i =n-1; i >=1 ; i--) {
             for (int j =i; j >=1 ; j--) {
                 System.out.print("*");
             }
