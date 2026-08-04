a26cfec6d190143c414b885f135f0c4b8d45d4c9
sauryaaman <amansaurya24@gmail.com>
2026-08-04 23:19:41 +0530

    CountAllDigit

---
+++ BasicMaths/CountAllDigitOfaNumber.java
@@ -0,0 +1,21 @@
+package BasicMaths;
+
+public class CountAllDigitOfaNumber{
+
+
+    CountAllDigitOfaNumber(int n)
+    {
+
+
+
+
+//       String s= String.valueOf(n);
+//       System.out.println(s.length());
+
+
+        String s=Integer.toString(n);
+        System.out.println(s.length());
+
+
+    }
+}
---
+++ BasicMaths/test.java
@@ -0,0 +1,7 @@
+package BasicMaths;
+
+public class test {
+    public static void main(String[] args) {
+        CountAllDigitOfaNumber c= new CountAllDigitOfaNumber(2435344);
+    }
+}
